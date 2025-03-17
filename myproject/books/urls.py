66f9da61c41efe68566c7b1592bff50c7326a251
Danutul0 <dan.ionescu779@gmail.com>
2025-03-17 20:29:05 +0200

    {commit_message}

--- myproject/books/urls.py
+++ myproject/books/urls.py
@@ -4,7 +4,8 @@ from . import views
 app_name = 'books'
 
 urlpatterns = [
-    path('', views.books_list, name ="list"),
-    path('add/', views.book_create, name ="book_add"),
-    path('<slug:slug>/', views.book_page, name ="book"),
+    path('', views.books_list, name='list'),
+    path('add/', views.book_create, name='book_add'),
+    path('filtered/', views.books_list_filtered, name='books_list_filtered'),
+    path('<slug:slug>/', views.book_page, name='book'),
 ]
--- myproject/books/views.py
+++ myproject/books/views.py
@@ -1,16 +1,14 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.utils.text import slugify
 from .models import Book
 from .forms import BookForm
-# Create your views here.
 
 def books_list(request):
     books = Book.objects.all().order_by('-date')
     return render(request, 'books/books_list.html', {'books': books})
 
-
 def book_page(request, slug):
-    book = Book.objects.get(slug=slug)
+    book = get_object_or_404(Book, slug=slug)
     return render(request, 'books/book_page.html', {'book': book})
 
 def book_create(request):
@@ -24,3 +22,11 @@ def book_create(request):
     else:
         form = BookForm()
     return render(request, 'books/book_form.html', {'form': form})
+
+def books_list_filtered(request):
+    title = request.GET.get('title', '')
+    if title:
+        books = Book.objects.filter(title__icontains=title).order_by('-date')
+    else:
+        books = Book.objects.all().order_by('-date')
+    return render(request, 'books/books_list_filtered.html', {'books': books, 'title': title})
